59edbfae027099da877d88c7e1029610d60f9476
eden essame <eden_essame80@hotmail.com>
2024-04-11 14:47:46 +0000

    outline the functions

--- run.py
+++ run.py
@@ -1,3 +1,40 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+class Board:
+    """
+    Main board class 
+    Ask for a name, only allow valid data
+    Create the player and computer boards 
+    Randomly place the ships
+    Print the boards
+    """
+    pass
+
+def guess_coodinates():
+    """
+    Ask user for coodinates
+    Only allow valid inputs
+    keep track of where has been guessed
+    Don't allow guesses outside the grid, or previously guessed
+    Update board with either a hit or a miss 
+    """
+    pass
+
+def computer_turn():
+    """
+    Computer to make a random selection
+    Make sure only valid selections
+    Update computers board with either a hit or miss
+    """
+    pass
+
+def count_ships_hit():
+    """
+    Keep track of the amount of both computer and player ships hit
+    If all computer ships are hit, the player wins
+    If all player ships are hit, the computer wins
+    """
+    pass
+
+
+
